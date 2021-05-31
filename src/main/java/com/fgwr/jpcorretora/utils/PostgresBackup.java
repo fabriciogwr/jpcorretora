86adf110c2e0e161f2e0166464bcff9964e31517
Fabrício Wagomacker <fabricio.gwr@gmail.com>
2021-05-31 03:51:23 -0400

    Configuração e cleanup de backup e restauração Limpeza no código Inserção de variável de ambiente de senha no código Drop schema antes de restaurar o bd

--- src/main/java/com/fgwr/jpcorretora/utils/PostgresBackup.java
+++ src/main/java/com/fgwr/jpcorretora/utils/PostgresBackup.java
@@ -4,34 +4,24 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.JOptionPane;
-
 import javafx.scene.control.Alert;
-import javafx.scene.control.DialogPane;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.DialogPane;
 import javafx.stage.StageStyle;
 
 public class PostgresBackup {
 
+	static String dir = System.getProperty("user.dir");
+
 	public static void realizaBackup(String path, String nome) throws IOException, InterruptedException {
 
-		final List<String> comandos = new ArrayList<String>();
-		comandos.add("C:\\Arquivos de programas\\PostgreSQL\\13\\bin\\pg_dump.exe"); // esse é meu caminho
-
-		comandos.add("-U");
-		comandos.add("postgres");
-		comandos.add("-w");
-		comandos.add("-F");
-		comandos.add("t");
-		comandos.add("-d");
-		comandos.add("jpcorretora");
-		comandos.add("-f");
-		comandos.add(path + "\\" + nome);
+		final List<String> comandos = List.of("C:\\Arquivos de programas\\PostgreSQL\\13\\bin\\pg_dump.exe", "-U",
+				"postgres", "-w", "-F", "t", "-d", "jpcorretora", "-f", path + "\\" + nome);
 
 		ProcessBuilder pb = new ProcessBuilder(comandos);
+		pb.environment().put("PGPASSWORD", "jpcorretora");
 
 		try {
 			final Process process = pb.start();
@@ -46,13 +36,14 @@ public class PostgresBackup {
 
 			process.waitFor();
 			process.destroy();
-			
+
 			Alert alert = new Alert(AlertType.INFORMATION);
 			alert.initStyle(StageStyle.UNIFIED);
 			DialogPane dialogPane = alert.getDialogPane();
 			dialogPane.getStylesheets().add(FilesUtils.fileToString(new File("css/alerts.css")));
 			alert.setTitle("Base de Dados Salva");
-			alert.setHeaderText("O Backup do banco de dados foi concluído. Em alguns instantes, a cópia dos dados adicionais será finalizada.");
+			alert.setHeaderText(
+					"O Backup do banco de dados foi concluído. Em alguns instantes, a cópia dos dados adicionais será finalizada.");
 			alert.showAndWait();
 
 		} catch (IOException e) {
@@ -64,18 +55,37 @@ public class PostgresBackup {
 	}
 
 	public static void realizaRestore(String input) throws IOException, InterruptedException {
-		final List<String> comandos = new ArrayList<String>();
+		final List<String> comandos = List.of("C:\\Arquivos de programas\\PostgreSQL\\13\\bin\\pg_restore.exe", "-U",
+				"postgres", "-d", "jpcorretora", input);
 
-		comandos.add("C:\\Arquivos de programas\\PostgreSQL\\13\\bin\\pg_restore.exe"); // testado no windows xp
+		final List<String> drops = List.of("C:\\Arquivos de programas\\PostgreSQL\\13\\bin\\psql.exe", "-U", "postgres",
+				"-d", "jpcorretora", "-f", dir + "\\postgres\\drops.sql");
 
-		comandos.add("-U");
-		comandos.add("postgres");
-		comandos.add("-d");
-		comandos.add("jpcorretora");
+		ProcessBuilder pbDrops = new ProcessBuilder(drops);
+		pbDrops.environment().put("PGPASSWORD", "jpcorretora");
 
-		comandos.add(input); // eu utilizei meu C:\ e D:\ para os testes e gravei o backup com sucesso.
+		try {
+			final Process process = pbDrops.start();
+
+			final BufferedReader r = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+			String line = r.readLine();
+			while (line != null) {
+				System.err.println(line);
+				line = r.readLine();
+			}
+			r.close();
+
+			process.waitFor();
+			process.destroy();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException ie) {
+			ie.printStackTrace();
+		}
 
 		ProcessBuilder pb = new ProcessBuilder(comandos);
+		pb.environment().put("PGPASSWORD", "jpcorretora");
 
 		try {
 			final Process process = pb.start();
@@ -90,7 +100,14 @@ public class PostgresBackup {
 
 			process.waitFor();
 			process.destroy();
-			JOptionPane.showMessageDialog(null, "Restore realizado com sucesso.");
+			Alert alert = new Alert(AlertType.INFORMATION);
+			alert.initStyle(StageStyle.UNIFIED);
+			DialogPane dialogPane = alert.getDialogPane();
+			dialogPane.getStylesheets().add(FilesUtils.fileToString(new File("css/alerts.css")));
+			alert.setTitle("Base de Dados Restaurada");
+			alert.setHeaderText(
+					"A restauração do banco de dados foi concluída. Em alguns instantes, a cópia dos dados adicionais será finalizada.");
+			alert.showAndWait();
 
 		} catch (IOException e) {
 			e.printStackTrace();
